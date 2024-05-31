aea380c0812334b09bdc4825d5c8b5c0e668eb6a
Joël Sanwiradi <joel@sanwiradi.com>
2024-05-31 17:31:50 +0200

    setup skeleton

--- src/test/java/com/kleineman85/abccompany/AbcCompanyApplicationTests.java
+++ src/test/java/com/kleineman85/abccompany/AbcCompanyApplicationTests.java
@@ -1,13 +1,26 @@
 package com.kleineman85.abccompany;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AbcCompanyApplicationTests {
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void myIntegrationTest() {
+		String url = "/api/v1/register";
+
+		restTemplate.postForObject(url, null, Object.class);
+		System.out.println("end of test");
+	}
+
 
 }
