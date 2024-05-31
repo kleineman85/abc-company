f6690ada19f668c64f9967cb4630eb0ffa7f50cf
Joël Sanwiradi <joel@sanwiradi.com>
2024-05-31 16:29:33 +0200

    initial commit

---
+++ src/main/java/com/kleineman85/abccompany/AbcCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.kleineman85.abccompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kleineman85/abccompany/AbcCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleineman85.abccompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
